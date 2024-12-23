a180c65fc5f62c9016f3415a42da12320b44bb2d
Zach Weiss <zachary.weiss86@gmail.com>
2024-12-23 10:54:12 -0600

    Cards can be played

--- game.php
+++ game.php
@@ -6,6 +6,24 @@ function saveGameState() {
     file_put_contents('game_state.json', json_encode($_SESSION['game']));
 }
 
+/**
+ * Summary of remEl
+ * @param mixed $keyword
+ * @param array $arr
+ * @return void
+ */
+function remEl($idx, $arr) {
+    $newarr = [];
+    foreach($arr as $val => $i) {
+        if($val == $idx) {
+            
+        } else {
+            array_push($newarr, $i);
+        }
+    }
+    return $newarr;
+}
+
 // Helper function to load the game state from a file
 function loadGameState() {
     if (file_exists('game_state.json')) {
@@ -70,8 +88,10 @@ function startGame() {
 
 // Play a turn (play a card)
 function playTurn($playerId, $card) {
-    if ($playerId !== $_SESSION['game']['current_turn']) {
-        return "It's not your turn!";
+    if ($playerId == $_SESSION['game']['current_turn']) {
+        
+    } else {
+        return $playerId;
     }
 
     $topCard = end($_SESSION['game']['discard_pile']);
@@ -80,10 +100,12 @@ function playTurn($playerId, $card) {
     }
 
     // Play the card
-    $_SESSION['game']['players'][$playerId]['hand'] = array_diff(
-        $_SESSION['game']['players'][$playerId]['hand'],
-        [$card]
-    );
+    //$_SESSION['game']['players'][$playerId]['hand'] = array_diff(
+    //    $_SESSION['game']['players'][$playerId]['hand'],
+    //    [$card]
+    //);
+    $_SESSION['game']['players'][$playerId]['hand'] = array_splice($_SESSION['game']['players'][$playerId]['hand'], array_search($card, $_SESSION['game']['players'][$playerId]['hand']),1);
+    //$_SESSION['game']['players'][$playerId]['hand'] = remEl(array_search($card, $_SESSION['game']['players'][$playerId]['hand']), $_SESSION['game']['players'][$playerId]['hand']);
     $_SESSION['game']['discard_pile'][] = $card;
 
     // Move to the next player
@@ -118,7 +140,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 break;
             case 'play':
                 if (isset($_POST['playerId']) && isset($_POST['card'])) {
-                    echo playTurn($_POST['playerId'], $_POST['card']);
+                    echo (playTurn($_POST['playerId'], $_POST['card']));
                 }
                 break;
             case 'state':
