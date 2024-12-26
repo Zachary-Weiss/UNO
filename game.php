8f6efa3ad39872c3f8d12858521c4a14855f1032
Zach Weiss <zachary.weiss86@gmail.com>
2024-12-26 10:56:12 -0600

    First usable version of uno!

--- game.php
+++ game.php
@@ -42,7 +42,7 @@ function loadGameState() {
 // Generate the UNO deck
 function generateDeck() {
     $colors = ['red', 'green', 'blue', 'yellow'];
-    $values = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'skip', 'reverse', 'draw2'];
+    $values = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'skip', 'reverse', 'draw2', 'draw4','draw8', 'draw40'];
     $deck = [];
 
     foreach ($colors as $color) {
@@ -82,21 +82,43 @@ function startGame() {
     $_SESSION['game']['discard_pile'][] = array_pop($_SESSION['game']['deck']);
 
     $_SESSION['game']['status'] = 'started';
+
+    $_SESSION['game']['direction'] = 1;
     saveGameState();
     return "Game started!";
+}
+
+function drawTurn($playerId) {
+    if ($playerId == $_SESSION['game']['current_turn']) {
+        // Check if the deck is empty
+        if (count($_SESSION['game']['deck']) > 0) {
+            // Draw a card and add it to the player's hand
+            $drawnCard = array_pop($_SESSION['game']['deck']);
+            $_SESSION['game']['players'][$playerId]['hand'][] = $drawnCard;
+
+            $_SESSION['game']['current_turn'] = ($_SESSION['game']['current_turn'] + 1) % count($_SESSION['game']['players']);
+            saveGameState();
+            
+            return "Card drawn!";
+        } else {
+            return "The deck is empty!";
+        }
+    } else {
+        return "IT'S NOT YOUR TURN!";
+    }
 }
 
 // Play a turn (play a card)
 function playTurn($playerId, $card) {
     if ($playerId == $_SESSION['game']['current_turn']) {
-        
+        // ...
     } else {
-        return $playerId;
+        return "ITS NOT YOUR TURN BRO ";
     }
 
     $topCard = end($_SESSION['game']['discard_pile']);
     if (!isValidMove($card, $topCard)) {
-        return "Invalid card!";
+        return "You can't play that card! ( Draw if no playable cards... )";
     }
 
     // Play the card
@@ -108,8 +130,83 @@ function playTurn($playerId, $card) {
     $_SESSION['game']['players'][$playerId]['hand'] = remEl(array_search($card, $_SESSION['game']['players'][$playerId]['hand']), $_SESSION['game']['players'][$playerId]['hand']);
     $_SESSION['game']['discard_pile'][] = $card;
 
+    if(explode('-', $card)[1] == "reverse") {
+        $_SESSION['game']['direction'] *= -1;
+    }
+
+    if(explode('-', $card)[1] == "draw2") {
+        $_SESSION['game']['players'][((($_SESSION['game']['current_turn'] + (1 * $_SESSION['game']['direction'])) % count($_SESSION['game']['players'])) + count($_SESSION['game']['players'])) % count($_SESSION['game']['players'])]['hand'][] = array_pop($_SESSION['game']['deck']);
+        $_SESSION['game']['players'][((($_SESSION['game']['current_turn'] + (1 * $_SESSION['game']['direction'])) % count($_SESSION['game']['players'])) + count($_SESSION['game']['players'])) % count($_SESSION['game']['players'])]['hand'][] = array_pop($_SESSION['game']['deck']);
+    }
+
+    if(explode('-', $card)[1] == "draw4") {
+        $_SESSION['game']['players'][((($_SESSION['game']['current_turn'] + (1 * $_SESSION['game']['direction'])) % count($_SESSION['game']['players'])) + count($_SESSION['game']['players'])) % count($_SESSION['game']['players'])]['hand'][] = array_pop($_SESSION['game']['deck']);
+        $_SESSION['game']['players'][((($_SESSION['game']['current_turn'] + (1 * $_SESSION['game']['direction'])) % count($_SESSION['game']['players'])) + count($_SESSION['game']['players'])) % count($_SESSION['game']['players'])]['hand'][] = array_pop($_SESSION['game']['deck']);
+        $_SESSION['game']['players'][((($_SESSION['game']['current_turn'] + (1 * $_SESSION['game']['direction'])) % count($_SESSION['game']['players'])) + count($_SESSION['game']['players'])) % count($_SESSION['game']['players'])]['hand'][] = array_pop($_SESSION['game']['deck']);
+        $_SESSION['game']['players'][((($_SESSION['game']['current_turn'] + (1 * $_SESSION['game']['direction'])) % count($_SESSION['game']['players'])) + count($_SESSION['game']['players'])) % count($_SESSION['game']['players'])]['hand'][] = array_pop($_SESSION['game']['deck']);
+    }
+
+    if(explode('-', $card)[1] == "draw8") {
+        $_SESSION['game']['players'][((($_SESSION['game']['current_turn'] + (1 * $_SESSION['game']['direction'])) % count($_SESSION['game']['players'])) + count($_SESSION['game']['players'])) % count($_SESSION['game']['players'])]['hand'][] = array_pop($_SESSION['game']['deck']);
+        $_SESSION['game']['players'][((($_SESSION['game']['current_turn'] + (1 * $_SESSION['game']['direction'])) % count($_SESSION['game']['players'])) + count($_SESSION['game']['players'])) % count($_SESSION['game']['players'])]['hand'][] = array_pop($_SESSION['game']['deck']);
+        $_SESSION['game']['players'][((($_SESSION['game']['current_turn'] + (1 * $_SESSION['game']['direction'])) % count($_SESSION['game']['players'])) + count($_SESSION['game']['players'])) % count($_SESSION['game']['players'])]['hand'][] = array_pop($_SESSION['game']['deck']);
+        $_SESSION['game']['players'][((($_SESSION['game']['current_turn'] + (1 * $_SESSION['game']['direction'])) % count($_SESSION['game']['players'])) + count($_SESSION['game']['players'])) % count($_SESSION['game']['players'])]['hand'][] = array_pop($_SESSION['game']['deck']);
+        $_SESSION['game']['players'][((($_SESSION['game']['current_turn'] + (1 * $_SESSION['game']['direction'])) % count($_SESSION['game']['players'])) + count($_SESSION['game']['players'])) % count($_SESSION['game']['players'])]['hand'][] = array_pop($_SESSION['game']['deck']);
+        $_SESSION['game']['players'][((($_SESSION['game']['current_turn'] + (1 * $_SESSION['game']['direction'])) % count($_SESSION['game']['players'])) + count($_SESSION['game']['players'])) % count($_SESSION['game']['players'])]['hand'][] = array_pop($_SESSION['game']['deck']);
+        $_SESSION['game']['players'][((($_SESSION['game']['current_turn'] + (1 * $_SESSION['game']['direction'])) % count($_SESSION['game']['players'])) + count($_SESSION['game']['players'])) % count($_SESSION['game']['players'])]['hand'][] = array_pop($_SESSION['game']['deck']);
+        $_SESSION['game']['players'][((($_SESSION['game']['current_turn'] + (1 * $_SESSION['game']['direction'])) % count($_SESSION['game']['players'])) + count($_SESSION['game']['players'])) % count($_SESSION['game']['players'])]['hand'][] = array_pop($_SESSION['game']['deck']);
+    }
+
+    if(explode('-', $card)[1] == "draw40") {
+        $_SESSION['game']['players'][((($_SESSION['game']['current_turn'] + (1 * $_SESSION['game']['direction'])) % count($_SESSION['game']['players'])) + count($_SESSION['game']['players'])) % count($_SESSION['game']['players'])]['hand'][] = array_pop($_SESSION['game']['deck']);
+        $_SESSION['game']['players'][((($_SESSION['game']['current_turn'] + (1 * $_SESSION['game']['direction'])) % count($_SESSION['game']['players'])) + count($_SESSION['game']['players'])) % count($_SESSION['game']['players'])]['hand'][] = array_pop($_SESSION['game']['deck']);
+        $_SESSION['game']['players'][((($_SESSION['game']['current_turn'] + (1 * $_SESSION['game']['direction'])) % count($_SESSION['game']['players'])) + count($_SESSION['game']['players'])) % count($_SESSION['game']['players'])]['hand'][] = array_pop($_SESSION['game']['deck']);
+        $_SESSION['game']['players'][((($_SESSION['game']['current_turn'] + (1 * $_SESSION['game']['direction'])) % count($_SESSION['game']['players'])) + count($_SESSION['game']['players'])) % count($_SESSION['game']['players'])]['hand'][] = array_pop($_SESSION['game']['deck']);
+        $_SESSION['game']['players'][((($_SESSION['game']['current_turn'] + (1 * $_SESSION['game']['direction'])) % count($_SESSION['game']['players'])) + count($_SESSION['game']['players'])) % count($_SESSION['game']['players'])]['hand'][] = array_pop($_SESSION['game']['deck']);
+        $_SESSION['game']['players'][((($_SESSION['game']['current_turn'] + (1 * $_SESSION['game']['direction'])) % count($_SESSION['game']['players'])) + count($_SESSION['game']['players'])) % count($_SESSION['game']['players'])]['hand'][] = array_pop($_SESSION['game']['deck']);
+        $_SESSION['game']['players'][((($_SESSION['game']['current_turn'] + (1 * $_SESSION['game']['direction'])) % count($_SESSION['game']['players'])) + count($_SESSION['game']['players'])) % count($_SESSION['game']['players'])]['hand'][] = array_pop($_SESSION['game']['deck']);
+        $_SESSION['game']['players'][((($_SESSION['game']['current_turn'] + (1 * $_SESSION['game']['direction'])) % count($_SESSION['game']['players'])) + count($_SESSION['game']['players'])) % count($_SESSION['game']['players'])]['hand'][] = array_pop($_SESSION['game']['deck']);
+        $_SESSION['game']['players'][((($_SESSION['game']['current_turn'] + (1 * $_SESSION['game']['direction'])) % count($_SESSION['game']['players'])) + count($_SESSION['game']['players'])) % count($_SESSION['game']['players'])]['hand'][] = array_pop($_SESSION['game']['deck']);
+        $_SESSION['game']['players'][((($_SESSION['game']['current_turn'] + (1 * $_SESSION['game']['direction'])) % count($_SESSION['game']['players'])) + count($_SESSION['game']['players'])) % count($_SESSION['game']['players'])]['hand'][] = array_pop($_SESSION['game']['deck']);
+        $_SESSION['game']['players'][((($_SESSION['game']['current_turn'] + (1 * $_SESSION['game']['direction'])) % count($_SESSION['game']['players'])) + count($_SESSION['game']['players'])) % count($_SESSION['game']['players'])]['hand'][] = array_pop($_SESSION['game']['deck']);
+        $_SESSION['game']['players'][((($_SESSION['game']['current_turn'] + (1 * $_SESSION['game']['direction'])) % count($_SESSION['game']['players'])) + count($_SESSION['game']['players'])) % count($_SESSION['game']['players'])]['hand'][] = array_pop($_SESSION['game']['deck']);
+        $_SESSION['game']['players'][((($_SESSION['game']['current_turn'] + (1 * $_SESSION['game']['direction'])) % count($_SESSION['game']['players'])) + count($_SESSION['game']['players'])) % count($_SESSION['game']['players'])]['hand'][] = array_pop($_SESSION['game']['deck']);
+        $_SESSION['game']['players'][((($_SESSION['game']['current_turn'] + (1 * $_SESSION['game']['direction'])) % count($_SESSION['game']['players'])) + count($_SESSION['game']['players'])) % count($_SESSION['game']['players'])]['hand'][] = array_pop($_SESSION['game']['deck']);
+        $_SESSION['game']['players'][((($_SESSION['game']['current_turn'] + (1 * $_SESSION['game']['direction'])) % count($_SESSION['game']['players'])) + count($_SESSION['game']['players'])) % count($_SESSION['game']['players'])]['hand'][] = array_pop($_SESSION['game']['deck']);
+        $_SESSION['game']['players'][((($_SESSION['game']['current_turn'] + (1 * $_SESSION['game']['direction'])) % count($_SESSION['game']['players'])) + count($_SESSION['game']['players'])) % count($_SESSION['game']['players'])]['hand'][] = array_pop($_SESSION['game']['deck']);
+        $_SESSION['game']['players'][((($_SESSION['game']['current_turn'] + (1 * $_SESSION['game']['direction'])) % count($_SESSION['game']['players'])) + count($_SESSION['game']['players'])) % count($_SESSION['game']['players'])]['hand'][] = array_pop($_SESSION['game']['deck']);
+        $_SESSION['game']['players'][((($_SESSION['game']['current_turn'] + (1 * $_SESSION['game']['direction'])) % count($_SESSION['game']['players'])) + count($_SESSION['game']['players'])) % count($_SESSION['game']['players'])]['hand'][] = array_pop($_SESSION['game']['deck']);
+        $_SESSION['game']['players'][((($_SESSION['game']['current_turn'] + (1 * $_SESSION['game']['direction'])) % count($_SESSION['game']['players'])) + count($_SESSION['game']['players'])) % count($_SESSION['game']['players'])]['hand'][] = array_pop($_SESSION['game']['deck']);
+        $_SESSION['game']['players'][((($_SESSION['game']['current_turn'] + (1 * $_SESSION['game']['direction'])) % count($_SESSION['game']['players'])) + count($_SESSION['game']['players'])) % count($_SESSION['game']['players'])]['hand'][] = array_pop($_SESSION['game']['deck']);
+        $_SESSION['game']['players'][((($_SESSION['game']['current_turn'] + (1 * $_SESSION['game']['direction'])) % count($_SESSION['game']['players'])) + count($_SESSION['game']['players'])) % count($_SESSION['game']['players'])]['hand'][] = array_pop($_SESSION['game']['deck']);
+        $_SESSION['game']['players'][((($_SESSION['game']['current_turn'] + (1 * $_SESSION['game']['direction'])) % count($_SESSION['game']['players'])) + count($_SESSION['game']['players'])) % count($_SESSION['game']['players'])]['hand'][] = array_pop($_SESSION['game']['deck']);
+        $_SESSION['game']['players'][((($_SESSION['game']['current_turn'] + (1 * $_SESSION['game']['direction'])) % count($_SESSION['game']['players'])) + count($_SESSION['game']['players'])) % count($_SESSION['game']['players'])]['hand'][] = array_pop($_SESSION['game']['deck']);
+        $_SESSION['game']['players'][((($_SESSION['game']['current_turn'] + (1 * $_SESSION['game']['direction'])) % count($_SESSION['game']['players'])) + count($_SESSION['game']['players'])) % count($_SESSION['game']['players'])]['hand'][] = array_pop($_SESSION['game']['deck']);
+        $_SESSION['game']['players'][((($_SESSION['game']['current_turn'] + (1 * $_SESSION['game']['direction'])) % count($_SESSION['game']['players'])) + count($_SESSION['game']['players'])) % count($_SESSION['game']['players'])]['hand'][] = array_pop($_SESSION['game']['deck']);
+        $_SESSION['game']['players'][((($_SESSION['game']['current_turn'] + (1 * $_SESSION['game']['direction'])) % count($_SESSION['game']['players'])) + count($_SESSION['game']['players'])) % count($_SESSION['game']['players'])]['hand'][] = array_pop($_SESSION['game']['deck']);
+        $_SESSION['game']['players'][((($_SESSION['game']['current_turn'] + (1 * $_SESSION['game']['direction'])) % count($_SESSION['game']['players'])) + count($_SESSION['game']['players'])) % count($_SESSION['game']['players'])]['hand'][] = array_pop($_SESSION['game']['deck']);
+        $_SESSION['game']['players'][((($_SESSION['game']['current_turn'] + (1 * $_SESSION['game']['direction'])) % count($_SESSION['game']['players'])) + count($_SESSION['game']['players'])) % count($_SESSION['game']['players'])]['hand'][] = array_pop($_SESSION['game']['deck']);
+        $_SESSION['game']['players'][((($_SESSION['game']['current_turn'] + (1 * $_SESSION['game']['direction'])) % count($_SESSION['game']['players'])) + count($_SESSION['game']['players'])) % count($_SESSION['game']['players'])]['hand'][] = array_pop($_SESSION['game']['deck']);
+        $_SESSION['game']['players'][((($_SESSION['game']['current_turn'] + (1 * $_SESSION['game']['direction'])) % count($_SESSION['game']['players'])) + count($_SESSION['game']['players'])) % count($_SESSION['game']['players'])]['hand'][] = array_pop($_SESSION['game']['deck']);
+        $_SESSION['game']['players'][((($_SESSION['game']['current_turn'] + (1 * $_SESSION['game']['direction'])) % count($_SESSION['game']['players'])) + count($_SESSION['game']['players'])) % count($_SESSION['game']['players'])]['hand'][] = array_pop($_SESSION['game']['deck']);
+        $_SESSION['game']['players'][((($_SESSION['game']['current_turn'] + (1 * $_SESSION['game']['direction'])) % count($_SESSION['game']['players'])) + count($_SESSION['game']['players'])) % count($_SESSION['game']['players'])]['hand'][] = array_pop($_SESSION['game']['deck']);
+        $_SESSION['game']['players'][((($_SESSION['game']['current_turn'] + (1 * $_SESSION['game']['direction'])) % count($_SESSION['game']['players'])) + count($_SESSION['game']['players'])) % count($_SESSION['game']['players'])]['hand'][] = array_pop($_SESSION['game']['deck']);
+        $_SESSION['game']['players'][((($_SESSION['game']['current_turn'] + (1 * $_SESSION['game']['direction'])) % count($_SESSION['game']['players'])) + count($_SESSION['game']['players'])) % count($_SESSION['game']['players'])]['hand'][] = array_pop($_SESSION['game']['deck']);
+        $_SESSION['game']['players'][((($_SESSION['game']['current_turn'] + (1 * $_SESSION['game']['direction'])) % count($_SESSION['game']['players'])) + count($_SESSION['game']['players'])) % count($_SESSION['game']['players'])]['hand'][] = array_pop($_SESSION['game']['deck']);
+        $_SESSION['game']['players'][((($_SESSION['game']['current_turn'] + (1 * $_SESSION['game']['direction'])) % count($_SESSION['game']['players'])) + count($_SESSION['game']['players'])) % count($_SESSION['game']['players'])]['hand'][] = array_pop($_SESSION['game']['deck']);
+        $_SESSION['game']['players'][((($_SESSION['game']['current_turn'] + (1 * $_SESSION['game']['direction'])) % count($_SESSION['game']['players'])) + count($_SESSION['game']['players'])) % count($_SESSION['game']['players'])]['hand'][] = array_pop($_SESSION['game']['deck']);
+        $_SESSION['game']['players'][((($_SESSION['game']['current_turn'] + (1 * $_SESSION['game']['direction'])) % count($_SESSION['game']['players'])) + count($_SESSION['game']['players'])) % count($_SESSION['game']['players'])]['hand'][] = array_pop($_SESSION['game']['deck']);
+        $_SESSION['game']['players'][((($_SESSION['game']['current_turn'] + (1 * $_SESSION['game']['direction'])) % count($_SESSION['game']['players'])) + count($_SESSION['game']['players'])) % count($_SESSION['game']['players'])]['hand'][] = array_pop($_SESSION['game']['deck']);
+        $_SESSION['game']['players'][((($_SESSION['game']['current_turn'] + (1 * $_SESSION['game']['direction'])) % count($_SESSION['game']['players'])) + count($_SESSION['game']['players'])) % count($_SESSION['game']['players'])]['hand'][] = array_pop($_SESSION['game']['deck']);
+    }
+
     // Move to the next player
-    $_SESSION['game']['current_turn'] = ($_SESSION['game']['current_turn'] + 1) % count($_SESSION['game']['players']);
+
+    if(explode('-', $card)[1] == "skip") {
+        $_SESSION['game']['current_turn'] = ((($_SESSION['game']['current_turn'] + (2 * $_SESSION['game']['direction'])) % count($_SESSION['game']['players'])) + count($_SESSION['game']['players'])) % count($_SESSION['game']['players']);
+    } else {
+        $_SESSION['game']['current_turn'] = ((($_SESSION['game']['current_turn'] + (1 * $_SESSION['game']['direction'])) % count($_SESSION['game']['players'])) + count($_SESSION['game']['players'])) % count($_SESSION['game']['players']);
+    }
     saveGameState();
     return "Card played!";
 }
@@ -130,6 +227,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     
     if (isset($_POST['action'])) {
         switch ($_POST['action']) {
+            case 'getid':
+                echo (count($_SESSION['game']['players']));
+                break; 
             case 'join':
                 if (isset($_POST['playerName'])) {
                     echo joinGame($_POST['playerName']);
@@ -141,6 +241,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             case 'play':
                 if (isset($_POST['playerId']) && isset($_POST['card'])) {
                     echo (playTurn($_POST['playerId'], $_POST['card']));
+                }
+                break;
+            case 'draw':
+                if ( isset( $_POST[ 'playerId' ] ) ) {
+                    echo (drawTurn($_POST['playerId']));
                 }
                 break;
             case 'state':
